75e81818e0d02c61ceb52ef552d5d3762dde19aa
Mathias Maaß <mathias.maass@uni-leipzig.de>
2020-12-31 19:44:30 +0100

    include lib

--- src/lisp.c
+++ src/lisp.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include "tokenize.h"
 #include "parse.h"
 #include "resolve.h"
 #include "stack.h"
 
-#define MAX_TEXT_LEN 100000
+#define MAX_FILE_SIZE 100000
 
 
-int get_text(char *buf, int max, FILE *f) {
+int read_file(char *buf, int max, FILE *f) {
     int i = 0,
         in_comment = 0;
-    char c;
+    char c; 
 
-    if (max < 1) {
-        return 0;
-    } 
-
-    while ((i < max - 1) && ((c = getc(f)) != EOF)) {
+    while ((i < max) && ((c = getc(f)) != EOF)) {
         if (in_comment) {
             if (c == '\n') {
                 buf[i++] = c;
@@ -32,32 +29,47 @@ int get_text(char *buf, int max, FILE *f) {
 
         }
     }
-    buf[i] = '\0';
-    return i - 1;
+
+    if (i >= max) {
+        printf("reached file size limit\n");
+        exit(1);
+    }
+
+    return i;
 }
 
-void print_result(Node *root) {
-    Node *child;
-    for (int i = 0; i < root->children->length; i++) {
-        child = list_get(root->children, i);
-        if (child->type != T_UND) {
-            printf("%s\n", child->val);
+char *get_text(char *include[]) {
+    int i = 0, text_size = 0; 
+    char *text = NULL, *cur, *fname;
+    FILE *f;
+
+    while ((fname = include[i++]) != NULL) {
+        f = fopen(fname, "r");
+        
+        if (!f) {
+            printf("can not open file %s\n", fname);
+            exit(1);
         }
+
+        text = realloc(text, text_size + MAX_FILE_SIZE + 1);
+        cur = text + text_size;
+        text_size += read_file(cur, MAX_FILE_SIZE, f);
+        fclose(f);
     }
+
+    if (text) {
+        text[text_size] = '\0';
+    }
+
+    return text;
 }
 
 int main(int argc, char *argv[]) {
-    int arg_print_stack = 0;
     int arg_print_ast = 0;
     char *arg_fname = NULL;
-    char text[MAX_TEXT_LEN];
-    FILE *f;
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-s") == 0) {
-            arg_print_stack = 1;
-        }
-        else if (strcmp(argv[i], "-a") == 0) {
+        if (strcmp(argv[i], "-a") == 0) {
             arg_print_ast = 1;
         }
         else {
@@ -65,19 +77,19 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    if (arg_fname) {
-        f = fopen(argv[1], "r");
-        if (!f) {
-            printf("could not open file %s\n", arg_fname);
-            return 0;
-        }
-    } else {
-        f = stdin;
+    if (!arg_fname) {
+        printf("usage: lisp [-a] FILE\n");
+        exit(1);
     }
 
-    get_text(text, MAX_TEXT_LEN, f);
-    printf("%s\n", text);
+    char *include[] = {
+        "lib/filter.dl",
+        arg_fname,
+        NULL // terminate list
+    };
 
+
+    char *text = get_text(include);
     List *tokens = tokenize(text);
     Node *root = parse(tokens);
     
@@ -87,16 +99,6 @@ int main(int argc, char *argv[]) {
         printf("\n");
     }
     
-    // Node *resolved_root = resolve(root);
     resolve(root);
-
-    if (arg_print_stack) {
-        printf("======== STACK ========\n");
-        print_stack();
-        printf("\n");
-    }
-    
-    // print_result(resolved_root);
-
     return 0;
 }
