409b626995f8493b9050a0bc77db0c09116f3207
Mathias Maaß <mathias.maass@uni-leipzig.de>
2021-01-11 19:15:36 +0100

    escape sequences in string

--- src/tokenize.c
+++ src/tokenize.c
@@ -26,6 +26,15 @@ static char *substring(char *s, int n) {
     return sub;
 }
 
+static char esc_seq_to_ascii(char c) {
+    switch (c) {
+        case 'n': return '\n'; break;
+        case 't': return '\t'; break;
+        case '\\': return '\\'; break;
+        default: return c; break;
+    }
+}
+
 List *tokenize(char* text) {
     List *tokens = list_create();
     Token *token;
@@ -88,11 +97,41 @@ List *tokenize(char* text) {
         // Any character between "" is considered a string.
         else if (*text == '"') {
             char *start = (text + 1);
+            int esc_len = 0; // length of escape sequences
             
-            while(*(++text) != '"')
-                ;
-            
-            char *form = substring(start, text - start);
+            // Forward the text until we get a closing quote
+            // and count the escape sequences by counting the backslashes.
+            // But dont count the backslash if the previous char is a backslash.
+            while(*(++text) != '"') {
+                if (*text == '\\' && *(text - 1) != '\\') {
+                    esc_len++;
+                }
+            }
+
+            // The string that we are going to extract will be as long
+            // as the characters between the quotes minus the backslashes
+            // plus one for the terminating null byte.
+            char *form = malloc(sizeof(char) * (text - start - esc_len + 1));
+            char *form_ptr = form;
+            int esc_on = 0;
+
+            // Copy the text chunk into the form string.
+            // If there is a backslash then skip copying the backslash
+            // and interpret the next character as an escape character.
+            for (int i = 0; i < text - start; i++) {
+                if (start[i] == '\\' && start[i - 1] != '\\') {
+                    esc_on = 1;
+                }
+                else if (esc_on) {
+                    *(form_ptr++) = esc_seq_to_ascii(start[i]);
+                    esc_on = 0;
+                }
+                else {
+                    *(form_ptr++) = start[i];
+                }
+            }
+
+            *form_ptr = '\0';
 
             text++;
             if (!is_valid_token_neighbor(*text)) {
